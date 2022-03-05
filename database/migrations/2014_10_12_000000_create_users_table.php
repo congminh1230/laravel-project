8380f8dafe2bfff4c8e86905536d4929369af637
congminh1230 <congminh2012002@gmail.com>
2022-03-05 16:35:16 +0700

    bai7.1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar');
+            $table->string('address');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->integer('status')->default();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
